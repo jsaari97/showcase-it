02f212c6b092e25ce5ef5213d9d901278e919a2f
Jim Saari <jim.saari97@gmail.com>
2020-05-28 00:14:36 +0300

    Add absolute paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { MainLayout } from "./components/layout/main";
+import { MainLayout } from "components/layout/main";
 
 export const App: React.FC = () => {
   return <MainLayout>Hello World!</MainLayout>;
