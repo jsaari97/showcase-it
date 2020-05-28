1d356c4029d77e82cade668b887a99a4c79ed4fe
Jim Saari <jim.saari97@gmail.com>
2020-05-28 14:06:57 +0300

    add animation loop

--- frontend/src/components/canvas/hook.tsx
+++ frontend/src/components/canvas/hook.tsx
@@ -13,9 +13,18 @@ export const useShowcaseCanvas = () => {
     )
   );
   const renderer = React.useRef(new THREE.WebGLRenderer());
+  const meshes = React.useRef<THREE.Object3D[]>([]);
 
   const animate = React.useCallback(() => {
     window.requestAnimationFrame(animate);
+
+    // animate
+    for (let i = 0; i < meshes.current.length; i++) {
+      const mesh = meshes.current[i];
+      mesh.rotation.x += 0.015;
+      mesh.rotation.y += 0.01;
+    }
+
     renderer.current.render(scene.current, camera.current);
   }, []);
 
@@ -26,6 +35,7 @@ export const useShowcaseCanvas = () => {
     const geometry = new THREE.BoxGeometry();
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
+    meshes.current = [...meshes.current, cube];
     scene.current.add(cube);
 
     camera.current.position.z = 5;
